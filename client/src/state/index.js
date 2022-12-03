203a0f07805398e682f9d254eba8473d985d8942
kevinsmasters <gmail@kevinmasters.com>
2022-12-02 20:01:31 -0500

    bug fix adventures. add friends list and profile view

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -24,7 +24,7 @@ export const authSlice = createSlice({
     },
     setFriends: (state, action) => {
       if (state.user) {
-        state.user.friends = action.payloads.friends;
+        state.user.friends = action.payload.friends;
       } else {
         console.error("user friends not existing");
       }
@@ -34,7 +34,7 @@ export const authSlice = createSlice({
     },
     setPost: (state, action) => {
       const updatedPosts = state.posts.map((post) => {
-        if (post._id === action.payload.post_id) return action.payload.post;
+        if (post._id === action.payload.post._id) return action.payload.post;
         return post;
       });
       state.posts = updatedPosts;
