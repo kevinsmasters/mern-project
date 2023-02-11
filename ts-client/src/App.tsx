1d9792f60b1fd9d2021a762f77e671f82eb9ddd8
kevinsmasters <gmail@kevinmasters.com>
2023-02-11 11:53:10 -0500

    base react install with typescript. adds homepage and routes

---
+++ ts-client/src/App.tsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LoginPage from "./pages/login";
+import HomePage from "./pages/home";
+import ProfilePage from "./pages/profile";
+
+function App() {
+  const isAuth = false;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
